5f70adeaa634bf4a201bc9441582799c6aef4eb1
Alaa Marei <alaamarei90@gmail.com>
2021-07-05 23:28:46 +0300

    create tree and add first info

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AboutmeProject
-new full project
+# LAB - 02
+
+## About me project 
+
+this is the second or third project i work on it by myself and it is a funny questions game to make comparison between my and yours answers .
+
+### Author: Alaa Marei
+
+
+### Links and Resources
+
+- [My gitHub acount](https://github.com/Alaa90-90)
+- [My Project](https://github.com/Alaa90-90/AboutmeProject)
+
+
+### Reflections and Comments
+
+this project updated daily ...
+and we will be happy to read your feedback
---
+++ js/app.js
@@ -0,0 +1,126 @@
+'use strict'
+
+
+let username=prompt('let us know about your NAME');
+console.log(username);
+
+
+console.log('the answer before',username);
+
+username=username.toLocaleUpperCase();
+
+console.log('the answer after',username);
+
+
+alert('nice to meet you, '+username );
+
+
+alert('LET US START THE GAME' );
+
+
+    let age=prompt('ARE YOU IN YOUR 20`S DECADE? YES OR NO ?');
+    console.log(age);
+
+    if (age==='YES') {
+
+    console.log(age);
+    alert('YOU ARE YOUNGMAN!');
+
+    } else if(age==='NO'){
+
+    console.log(age);
+    alert('YOU ARE NOT IN 20`S AND MAYBE NOT CONCERN IN FOOTBALL');
+
+    }else {
+    alert('please write your answer in UPPERCASE');
+    }
+
+
+        let home=prompt('ARE YOU LIVE IN AMMAN ?');
+        console.log(home);
+
+
+        if (home==='YES') {
+
+        console.log(home);
+        alert('I LIVE IN AMMAN TOO!');
+
+        } else if(home==='NO'){
+
+        console.log(home);
+        alert('SO YOU LIVE IN OTHER PROVINCE');
+
+        }else {
+        alert('please write your answer in UPPERCASE');
+        }
+
+
+            let football=prompt('DO YOU LIKE WATCH FOOTBALL ?');
+            console.log(football);
+
+
+            if (football==='YES') {
+
+            console.log(football);
+            alert('SO YOU LIKE FOOTBALL');
+
+            } else if(football==='NO'){
+
+            console.log(football);
+            alert('MAYBE YOU LIKE ANOTHER SPORT');
+
+            }else {
+            alert('please write your answer in UPPERCASE');
+            }
+
+
+                let best=prompt('DO YOU THINK ! LEONIL MESSI ! IS THE BEST FOOTBALL EVER ?');
+                console.log(best);
+
+                if (best==='YES') {
+
+                console.log(best);
+                alert('I THINK THAT TOO ');
+
+                } else if(best==='NO'){
+
+                console.log(best);
+                alert(' I DO NOT THINK THAT AND I DON`T AGREE WITH YOU');
+
+                }else {
+                alert('please write your answer in UPPERCASE');
+                }
+
+
+                    let copa=prompt(' DO YOU THINK ! LEONIL MESSI ! WILL GET COPA AMERICA THIS YEAR ?');
+                    console.log(copa);
+
+
+                    if (copa==='YES') {
+
+                    console.log(copa);
+                    alert('OF  COURSE I AM SURE LIKE YOU ! ');
+
+                    } else if(copa==='NO'){
+
+                    console.log(copa);
+                    alert('LET US WAIT AND SEE IT JUST A FEW DAYS ');
+
+
+                    }else {
+                    alert('please write your answer in UPPERCASE');
+                    }
+
+
+                        if(best==='YES'){
+                        alert('YOU ARE REAL FOOTBALL FAN' );
+                        }else{
+                        alert('I THINK ANIME IS GOOD FOR YOU ' );
+                        }
+
+
+
+
+
+
+
