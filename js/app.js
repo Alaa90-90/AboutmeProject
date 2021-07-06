9434779af8745d0f859219e1853d8197de53d86b
Alaa Marei <alaamarei90@gmail.com>
2021-07-06 14:20:53 +0300

    fix the problem in the code

--- js/app.js
+++ js/app.js
@@ -1,122 +1,157 @@
-'use strict'
+'use strict';
 
 
 let username=prompt('let us know about your NAME');
 console.log(username);
 
 
-console.log('the answer before',username);
-
-username=username.toLocaleUpperCase();
-
-console.log('the answer after',username);
-
 
 alert('nice to meet you, '+username );
 
 
-alert('LET US START THE GAME' );
+alert('LET US START THE GUESSING GAME' );
 
 
-    let age=prompt('ARE YOU IN YOUR 20`S DECADE? YES OR NO ?');
+let myname=prompt('DO YOU THINK MY NAME START WITH .. A ..?');
+
+console.log('the answer before',myname);
+
+myname=myname.toUpperCase();
+
+console.log('the answer after',myname);
+
+if (myname ==='YES'|| myname ==='Y') {
+
+console.log(myname);
+alert('YOU ARE RIGHT!');
+
+} else if(myname ==='NO'|| myname ==='N'){
+
+console.log(myname);
+alert('TRY AGAIN ..');
+
+}else {
+alert('please write your answer using  yes/no or y/n only !');
+}
+
+
+    let age=prompt('DO YOU THINK THAT I AM IN THE 20`S DECADE? ');
+
+    console.log('the answer before',age);
+
+    age=age.toUpperCase();
+    
+    console.log('the answer after',age);
+
+    if (age ==='YES'|| age ==='Y') {
+
     console.log(age);
+    alert('MAYBE!');
 
-    if (age==='YES') {
-
-    console.log(age);
-    alert('YOU ARE YOUNGMAN!');
-
-    } else if(age==='NO'){
+    } else if(age==='NO'|| age ==='N'){
 
     console.log(age);
-    alert('YOU ARE NOT IN 20`S AND MAYBE NOT CONCERN IN FOOTBALL');
+    alert('IT SEEM RIGHT');
 
     }else {
-    alert('please write your answer in UPPERCASE');
+    alert('please write your answer using  yes/no or y/n only !');
     }
 
 
-        let home=prompt('ARE YOU LIVE IN AMMAN ?');
+        let home=prompt('IN YOUR OPINION I LIVE IN AMMAN ?');
+        
+            console.log('the answer before',home);
+
+            home=home.toUpperCase();
+            
+            console.log('the answer after',home);
+
+        if (home ==='YES'|| home ==='Y') {
+
         console.log(home);
+        alert('YES I LIVE IN AMMAN !');
 
-
-        if (home==='YES') {
-
-        console.log(home);
-        alert('I LIVE IN AMMAN TOO!');
-
-        } else if(home==='NO'){
+        } else if(home ==='NO'|| home ==='N'){
 
         console.log(home);
-        alert('SO YOU LIVE IN OTHER PROVINCE');
+        alert('IN FACT I LIVE IN AMMANH');
 
         }else {
-        alert('please write your answer in UPPERCASE');
+        alert('please write your answer using  yes/no or y/n only !');
         }
 
 
-            let football=prompt('DO YOU LIKE WATCH FOOTBALL ?');
+            let football=prompt('DO YOU THINK THAT I LIKE WATCH FOOTBALL ?');
+
+            console.log('the answer before',football);
+
+            football=football.toUpperCase();
+            
+            console.log('the answer after',football);
+
+            if (football ==='YES'|| football ==='Y') {
+
             console.log(football);
+            alert('YES I LIKE WATCH FOOTBALL');
 
-
-            if (football==='YES') {
-
-            console.log(football);
-            alert('SO YOU LIKE FOOTBALL');
-
-            } else if(football==='NO'){
+            } else if(football ==='NO'|| football ==='N'){
 
             console.log(football);
-            alert('MAYBE YOU LIKE ANOTHER SPORT');
+            alert('YOU ARE WRONG !');
 
             }else {
-            alert('please write your answer in UPPERCASE');
+            alert('please write your answer using  yes/no or y/n only !');
             }
 
 
-                let best=prompt('DO YOU THINK ! LEONIL MESSI ! IS THE BEST FOOTBALL EVER ?');
+                let best=prompt('DO YOU AGREE WITH ME THAT ! LEONIL MESSI ! IS THE BEST FOOTBALL EVER ?');
+
+                console.log('the answer before',best);
+
+                best=best.toUpperCase();
+                
+                console.log('the answer after',best);
+
+                if (best ==='YES'|| best ==='Y') {
+
                 console.log(best);
+                alert('YOU ARE REAL FOOTBALL FAN ');
 
-                if (best==='YES') {
-
-                console.log(best);
-                alert('I THINK THAT TOO ');
-
-                } else if(best==='NO'){
+                } else if(best==='NO'|| best ==='N'){
 
                 console.log(best);
                 alert(' I DO NOT THINK THAT AND I DON`T AGREE WITH YOU');
 
                 }else {
-                alert('please write your answer in UPPERCASE');
+                alert('please write your answer using  yes/no or y/n only !');
                 }
 
 
-                    let copa=prompt(' DO YOU THINK ! LEONIL MESSI ! WILL GET COPA AMERICA THIS YEAR ?');
-                    console.log(copa);
+                    // let copa=prompt(' DO YOU THINK ! LEONIL MESSI ! WILL GET COPA AMERICA THIS YEAR ?');
+                    // console.log(copa);
 
 
-                    if (copa==='YES') {
+                    // if (copa==='YES') {
 
-                    console.log(copa);
-                    alert('OF  COURSE I AM SURE LIKE YOU ! ');
+                    // console.log(copa);
+                    // alert('OF  COURSE I AM SURE LIKE YOU ! ');
 
-                    } else if(copa==='NO'){
+                    // } else if(copa==='NO'){
 
-                    console.log(copa);
-                    alert('LET US WAIT AND SEE IT JUST A FEW DAYS ');
-
-
-                    }else {
-                    alert('please write your answer in UPPERCASE');
-                    }
+                    // console.log(copa);
+                    // alert('LET US WAIT AND SEE IT JUST A FEW DAYS ');
 
 
-                        if(best==='YES'){
-                        alert('YOU ARE REAL FOOTBALL FAN' );
-                        }else{
-                        alert('I THINK ANIME IS GOOD FOR YOU ' );
-                        }
+                    // }else {
+                    // alert('please write your answer in UPPERCASE');
+                    // }
+
+
+                    //     if(best==='YES'){
+                    //     alert('YOU ARE REAL FOOTBALL FAN' );
+                    //     }else{
+                    //     alert('I THINK ANIME IS GOOD FOR YOU ' );
+                    //     }
 
 
 
